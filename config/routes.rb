ac4ceeabba76f109b3cde252d2afcbca39c86e03
Aaron Grando <aaron.grando@gmail.com>
2013-06-17 16:20:23 -0400

    activeadmin fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Boilerplate::Application.routes.draw do
-  
-  ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
@@ -8,5 +6,7 @@ Boilerplate::Application.routes.draw do
   # earlier in this file -> higher priority.
 
   root to: 'home#index', as: :home
+  
+  ActiveAdmin.routes(self)
 
 end
